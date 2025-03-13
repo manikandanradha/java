7005f8cce07eacd2ad24e933413204d48a1ad318
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 13:36:21 +0530

    l

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+
+ public static void main(String[] args) {
+
+  int x = 10;
+  int y = 20;
+
+  System.out.println("Before Swapping");
+  System.out.println("Value of x is :" + x);
+  System.out.println("Value of y is :" + y);
+  x = x + y;
+  y = x - y;
+  x = x - y;
+
+  System.out.println("after swapping");
+  System.out.println("Value of x is :" + x);
+  System.out.println("Value of y is :" + y);
+ }
+}
