fee1a81a185bad6b88f216551e44bf9595fd575f
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 15:45:37 +0530

    added day

---
+++ switching.java
@@ -0,0 +1,38 @@
+public class switching{
+    public static void main(String[]args){
+        int day=3;
+        switch (day)
+        {
+            case 1 : System.out.println("the day is monday");
+            break;
+        
+        
+            case 2 : System.out.println("the day is tuesday");
+            break;
+        
+        
+            case 3 : System.out.println("the day is wednesday");
+            break;
+        
+        
+            case 4 : System.out.println("the day is thursday");
+            break;
+        
+        
+            case 5 : System.out.println("the day is friday");
+            break;
+        
+        
+            case 6 : System.out.println("the day is saturday");
+            break;
+        
+        
+            case 7 : System.out.println("the day is sunday");
+            break;
+        
+        default:
+            case 8 : System.out.println("this is not a day");
+        }
+
+    }
+}
