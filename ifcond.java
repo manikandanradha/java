e98618fb2d136a60dc3d4a267a6ee6bbf6e1f489
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 14:35:50 +0530

    k

---
+++ ifcond.java
@@ -0,0 +1,9 @@
+public class ifcond{
+    public static void main(String[]args){
+        int a=16;
+        int b=18;
+        if(a > b){
+            System.out.println("16 is less than 20");
+        }
+    }
+}
