3332aac28152bae13bbc053ba9d473f807c0dcfd
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 13:53:21 +0530

    l

---
+++ about.java
@@ -0,0 +1,10 @@
+public class about{
+    public static void main(String[]args){
+        String empname ="mani";
+        int mark = 85;
+        char grade = 'B';
+        System.out.println("enter the name:"+ empname);
+        System.out.println("enter the mark:"+ mark);
+        System.out.println("enter the grade:"+ grade);
+    }
+}
