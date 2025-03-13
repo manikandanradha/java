874e33cdb8ae75ac22eb8bc47bcfd5ebe3abef34
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 13:56:29 +0530

    addition

---
+++ operators.java
@@ -0,0 +1,12 @@
+public class operators{
+    public static void main(String[]args){
+        int a=8;
+        int b=6;
+        int Addition=a+b;
+        int Subtraction=a-b;
+        int multiplication=a*b;
+        System.out.println(Addition);
+        System.out.println(Subtraction);
+        System.out.println(multiplication);
+    }
+}
