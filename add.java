985df3d15f11e9810b4d2361f9ca236f1a7672a3
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 15:20:14 +0530

    added names

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add
+{
+    public static void main(String[]args){
+        String name1 = "manii";
+        String name2 = "sumii";
+        System.out.println(name1.concat(name2));
+
+    }
+}
