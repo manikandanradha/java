077c7ec9c4875242b001601702628aaa6d873013
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 15:16:25 +0530

    added grade

---
+++ elsecond.java
@@ -0,0 +1,17 @@
+public class elsecond{
+    public static void main(String[]args){
+        int mark=90;
+        if(mark>=90){
+            System.out.println("Grade A");
+        }
+        else if (mark>=80){
+            System.out.println("Grade B");
+        }
+        else if (mark>=70){
+            System.out.println("Grade C");
+        }
+        else if (mark>=60){
+            System.out.println("Grade D");
+        }
+    }
+}
