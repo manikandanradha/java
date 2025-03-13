664a2dde8a4a9c5a7b506d8d40a22fd0e775bcbb
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 13:54:52 +0530

    added and or

---
+++ andor.java
@@ -0,0 +1,8 @@
+public class andor{
+    public static void main(String[]args){
+        int a=60;
+        int b=40;
+        System.out.println(a>b && a==b);
+        System.out.println(a>b || a==b);
+    }
+}
