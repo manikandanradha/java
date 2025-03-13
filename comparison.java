581bf5ca5e759182dc45195e129688e89706fe1a
manikandanradha <manikandanradha15@gmail.com>
2025-03-13 13:55:38 +0530

    added comparison of a and b

---
+++ comparison.java
@@ -0,0 +1,10 @@
+public class comparison{
+    public static void main(String[]args){
+        String name1 = "manikandan";
+        String name2 = "rahul";
+        int a=5;
+        int b=7;
+        System.out.println(name1==name2);
+        System.out.println(a<b);
+    }
+}
